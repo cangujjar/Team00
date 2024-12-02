4a0c8e3354be268730869babcae268478480424e
Anshu Nagar <cangujjar@gmail.com>
2024-12-02 12:17:32 +0530

    Initial Commit

---
+++ Team00Tests/Team00Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Team00Tests.swift
+//  Team00Tests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import Team00
+
+struct Team00Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
